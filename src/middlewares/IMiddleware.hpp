a5a9a7bd77b9db593c6dc1d25f7e6254807ab8ac
carloscncdev <carloscndev@gmail.com>
2026-06-26 17:41:39 -0600

    fix: typos

--- src/middlewares/IMiddleware.hpp
+++ src/middlewares/IMiddleware.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include "../http/HttpRequest.hpp"
 #include "../http/HttpResponse.hpp"
-#include<optional>
-#include<functional>
+#include <optional>
+#include <functional>
 
 class IMiddleware {
-    public:
-        virtual ~IMiddleware = default;
+public:
+    // Virtual destructor ensures derived class resources are cleaned up safely
+    virtual ~IMiddleware() = default;
 
-        virtual std::optional<HttpResponse> execute(cont HttpRequest& req) = 0;
+    // If it returns an HttpResponse, it breaks the pipeline (Early Return, e.g., 401 Unauthorized)
+    // If it returns std::nullopt, the execution continues to the next step
+    virtual std::optional<HttpResponse> execute(const HttpRequest& req) = 0;
 };
